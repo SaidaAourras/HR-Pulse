a1931e86d169d580aba0f25ceb0f6a5abc196685
SaidaAourras <sa123aouras@gmail.com>
2026-02-26 22:55:12 +0000

    feat(routes): update predict route (TF-IDF desc + title + catégoriels)

--- backend/app/api/routes/predict.py
+++ backend/app/api/routes/predict.py
@@ -1,31 +1,21 @@
-import json
-import pandas as pd
 from fastapi import APIRouter, HTTPException
-from backend.app.api.db.schemas.predict import PredictResponse , PredictRequest
-from backend.app.services.predict_services import load_artifacts
+from backend.app.api.db.schemas.predict import PredictRequest, PredictResponse
+from backend.app.services.predict_services import predict
+
+predict_router = APIRouter(prefix="/predict", tags=["Predict"])
 
 
-predict_router = APIRouter(prefix="/predict" , tags=["Predict"])
-
-
-@predict_router.post("/" , response_model=PredictResponse)
+@predict_router.post("/", response_model=PredictResponse)
 def predict_salary(req: PredictRequest):
-    model , encoder , meta = load_artifacts()
-    
-    X = encoder.transform(pd.DataFrame([[req.sector, req.size, req.type_of_ownership, req.state]], columns=meta["cat_cols"])) 
-    
-    prediction = round(float(model.predict(X)[0]),1)
-    
-    return PredictResponse(
-        predicted_salary_k = prediction,
-        mae_k              = meta["mae_k"],
-        salary_mean_k      = meta["salary_mean"],
-        salary_min_k       = meta["salary_min"],
-        salary_max_k       = meta["salary_max"],
+    try:
+        result = predict(
+            job_title         = req.job_title,
+            job_description   = req.job_description,
+            sector            = req.sector,
+            size              = req.size,
+            type_of_ownership = req.type_of_ownership,
+            state             = req.state,
         )
-
-
-
-
-
-
+        return PredictResponse(**result)
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
